1fadeaf3e237763f384d085e27d3ee8183323392
Sergio Flores Ramírez <serchfr890123@gmail.com>
2020-10-08 12:31:53 -0500

    Initial Commit

---
+++ Tweets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tweets
+//
+//  Created by Axity on 08/10/20.
+//  Copyright © 2020 Platzi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
