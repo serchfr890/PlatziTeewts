ccaddbb978901f0e6342e2e0d768d32341b372f7
Sergio Flores Ramírez <serchfr890123@gmail.com>
2020-10-19 20:41:40 -0500

    se realiza cambios para obtener y eliminar los tweets

---
+++ Tweets/Models/Response/GeneralResponse.swift
@@ -0,0 +1,13 @@
+//
+//  GeneralResponse.swift
+//  Tweets
+//
+//  Created by Axity on 08/10/20.
+//  Copyright © 2020 Platzi. All rights reserved.
+//
+
+import Foundation
+struct GeneralResponse: Codable {
+    let isDone: Bool
+    let message: String
+}
--- Tweets/Modules/Home/HomeViewController.swift
+++ Tweets/Modules/Home/HomeViewController.swift
@@ -32,6 +32,7 @@ class HomeViewController: UIViewController {
     // MARK: Private Methods
     private func setupUI() {
         tableView.dataSource = self
+        tableView.delegate = self
         tableView.register(UINib(nibName: cellId, bundle: nil), forCellReuseIdentifier: cellId)
     }
     private func getPost() {
@@ -49,8 +50,30 @@ class HomeViewController: UIViewController {
             }
         }
     }
+    private func deletePost(indexPath: IndexPath?) {
+        SVProgressHUD.show()
+        guard let index = indexPath?.row, index >= 0 else {
+            SVProgressHUD.dismiss()
+            NotificationBanner(title: "Error", subtitle: "Hubo un error al eliminar el Tweet", style: .success).show()
+            return
+        }
+        let postId = self.dataSource[index].id
+        let endpoint = Endpoints.post + "/\(postId)"
+        
+        SN.delete(endpoint: endpoint) { (response: SNResultWithEntity<GeneralResponse, ErrorResponse>) in
+            SVProgressHUD.dismiss()
+            switch response {
+            case .success:
+                self.dataSource.remove(at: index)
+                self.tableView.deleteRows(at: [indexPath!], with: .top)
+            case .error(let error):
+                NotificationBanner(title: "Error",subtitle: "\(error.localizedDescription)", style: .danger).show()
+            case .errorResult(let entity):
+                NotificationBanner(title: "Error",subtitle: "Hubo un error: \(entity.error)", style: .danger).show()
+            }
+        }
+    }
 }
-
 extension HomeViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return dataSource.count
@@ -61,5 +84,16 @@ extension HomeViewController: UITableViewDataSource {
             cell.setupCellWith(post: dataSource[indexPath.row])
         }
         return cell
+    }
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return dataSource[indexPath.row].author.email == "serchtest@test.com"
+    }
+}
+extension HomeViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let deleteAction = UITableViewRowAction(style: .destructive, title: "Borrar") { (_, _) in
+            self.deletePost(indexPath: indexPath)
+        }
+        return [deleteAction]
     }
 }
